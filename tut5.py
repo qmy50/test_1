feb4466f932d8c8d54efeb8ddc8a669081d9e078
qmy50 <m18515251715@163.com>
2025-05-09 23:23:56 +0800

    gradient in

--- tut5.py
+++ tut5.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from PIL import Image
+import house as h
 
-st.write('机器学习作业展示：')
+
+st.title('机器学习作业展示：')
 
 if "step" not in st.session_state:
     st.session_state['step']=1
@@ -10,7 +12,7 @@ def goto_step(step_num:int):
     st.session_state['step']=step_num
 
 if st.session_state.step==1:
-    st.title('算法选择')
+    st.subheader('算法选择')
     st.button(label='梯度下降——房价问题',on_click=goto_step,args=((2,)))
     st.button(label='SVM——鸢尾花分类',on_click=goto_step,args=((3,)))
 
@@ -31,7 +33,7 @@ x_train,x_test,y_train,y_test = train_test_split(iris.data, iris.target) """
           max_value=150,
           value=0,
           step=1)
-    ans=2*slider_val+3
+    ans=h.my_k*slider_val+h.my_b
 
     st.write("预测房价结果为：",'%.2f' % ans)
 
